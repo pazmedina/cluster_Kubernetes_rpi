d0f0170d44a19fe22b7dfca4f3a24fb7cf610849
pazmedina <pazmedina@gmail.com>
2022-03-23 15:33:11 +0100

    añadimos workers

--- Bootstrap/bootstrap.tf
+++ Bootstrap/bootstrap.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "cluster_bootstrap" {
+resource "null_resource" "cluster_bootstrap_master" {
   for_each = local.master
   connection {
     type        = "ssh"
@@ -23,6 +23,35 @@ inline = [
  
     # Arrancamos cluster
      # "sudo microk8s.start"
+]
+
+}
+}
+
+resource "null_resource" "cluster_bootstrap_workers" {
+  for_each = local.workers
+  connection {
+    type        = "ssh"
+    user        = var.user
+    private_key = file("~/.ssh/${var.key}")
+    host        = each.value.ip_addr
+  }
+
+
+provisioner "remote-exec" {
+inline = [
+    #Actualizar los nodos del cluster
+      "echo ${var.pw_root} | sudo -S apt-get update",
+      "sudo apt-get install -y curl",
+
+    # Realizamos modificaciones en el fichero /boot/firmware/cmdline.txt
+      "if ! grep -qP 'cgroup_enable=memory' /boot/firmware/cmdline.txt; then sudo sed -i.bck '$s/$/ cgroup_enable=memory/' /boot/firmware/cmdline.txt; fi",
+      "if ! grep -qP 'cgroup_memory=1' /boot/firmware/cmdline.txt; then sudo sed -i.bck '$s/$/ cgroup_memory=1/' /boot/firmware/cmdline.txt; fi",
+      
+    # Instalamos microk8s
+      "sudo snap install microk8s --classic",
+ 
+   
 ]
 
 }
