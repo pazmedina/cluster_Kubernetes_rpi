0005b2c600fa233a34b5bb05be4d63afc1d064f2
pazmedina <pazmedina@gmail.com>
2022-03-23 11:03:58 +0100

    corregir error

--- Bootstrap/main.tf
+++ Bootstrap/main.tf
@@ -9,7 +9,7 @@ locals {
       hostname = "Kubemaster"
       ip_addr  = "192.168.1.142"
       role     = ["controlplane", "worker", "etcd"]
-    ,
+      }
     node01 = {
       hostname = "Kubenode01"
       ip_addr  = "192.168.1.139"
@@ -17,4 +17,5 @@ locals {
     }
   }
 
+}
 }
