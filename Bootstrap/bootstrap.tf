e454519f047a8bd22e2e781be65f4a5eeeef9ab4
pazmedina <pazmedina@gmail.com>
2022-03-23 11:31:24 +0100

    modificaciones cmdline.txt

--- Bootstrap/bootstrap.tf
+++ Bootstrap/bootstrap.tf
@@ -10,8 +10,14 @@ resource "null_resource" "cluster_bootstrap" {
 
 provisioner "remote-exec" {
 inline = [
+    #Actualizar los nodos del cluster
       "echo ${var.pw_root} | sudo -S apt-get update",
       "sudo apt-get install -y curl",
+
+    # Realizamos modificaciones en el fichero /boot/firmware/cmdline.txt
+      "if ! grep -qP 'cgroup_enable=memory' /boot/firmware/cmdline.txt; then sudo sed -i.bck '$s/$/ cgroup_enable=memory/' /boot/firmware/cmdline.txt; fi",
+      "if ! grep -qP 'cgroup_memory=1' /boot/firmware/cmdline.txt; then sudo sed -i.bck '$s/$/ cgroup_memory=1/' /boot/firmware/cmdline.txt; fi",
+      
 ]
 
 }
