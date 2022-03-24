4a376563c51fea4a7dcc7838b0b5519f4bfd6908
pazmedina <pazmedina@gmail.com>
2022-03-24 22:36:19 +0100

    cambios

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -3,8 +3,38 @@ provider "kubernetes" {
   config_context = "my-context"
 }
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "test" {
   metadata {
     name = "my-first-namespace"
+  }
+}
+resource "kubernetes_deployment" "test" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "MyTestApp"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "MyTestApp"
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
   }
 }
