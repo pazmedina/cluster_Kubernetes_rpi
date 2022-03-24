f3c0a4dcfd7939f0edee60a4d486ee91faae6246
pazmedina <pazmedina@gmail.com>
2022-03-24 22:47:31 +0100

    corregir error

--- Pipeline/main.tf
+++ Pipeline/main.tf
@@ -16,7 +16,7 @@ resource "kubernetes_namespace" "test" {
 }
 resource "kubernetes_deployment" "test" {
   metadata {
-    name      = "nginx_test"
+    name      = "nginxtest"
     namespace = kubernetes_namespace.test.metadata.0.name
   }
   spec {
